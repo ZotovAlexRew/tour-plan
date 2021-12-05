f50e53409831b1ad7b148a2da3d161aa3ee1d34b
zotovalexrew <che123zotov@gmail.com>
2021-12-05 18:47:28 +0300

    create: madel window and add closed this window

--- js/main.js
+++ js/main.js
@@ -1,34 +1,65 @@
-const hotelSlider = new Swiper('.hotel-slider', {
-  // Optional parameters
-  loop: true,
+$(document).ready(function (){
+  const hotelSlider = new Swiper('.hotel-slider', {
+    // Optional parameters
+    loop: true,
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.hotel-slider__button--next',
-    prevEl: '.hotel-slider__button--prev',
-  },
+    // Navigation arrows
+    navigation: {
+      nextEl: '.hotel-slider__button--next',
+      prevEl: '.hotel-slider__button--prev',
+    },
 
-  keyboard: {
-    enabled: true,
-    onlyInViewport: false,
-  },
-});
+    keyboard: {
+      enabled: true,
+      onlyInViewport: false,
+    },
+  });
 
-const reviewsSlider = new Swiper('.reviews-slider', {
-  // Optional parameters
-  loop: true,
+  const reviewsSlider = new Swiper('.reviews-slider', {
+    // Optional parameters
+    loop: true,
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.reviews-slider__button--next',
-    prevEl: '.reviews-slider__button--prev',
-  },
+    // Navigation arrows
+    navigation: {
+      nextEl: '.reviews-slider__button--next',
+      prevEl: '.reviews-slider__button--prev',
+    },
 
-});
+  });
 
-var menuButton = document.querySelector(".menu-button");
-menuButton.addEventListener("click", function(){
-  console.log('Клик по кнопке меню');
-  document.querySelector(".navbar-button")
-  .classList.toggle("navbar-button--visible");
+  var menuButton = document.querySelector(".menu-button");
+  menuButton.addEventListener("click", function(){
+    console.log('Клик по кнопке меню');
+    document.querySelector(".navbar-button")
+    .classList.toggle("navbar-button--visible");
+  });
+
+  var modelButton = $("[data-toggle=model]");
+  var closeModelButton = $(".model__close");
+  modelButton.on("click", openModel);
+  closeModelButton.on("click", closeModel);
+
+  function openModel() {
+    var modelOverlay = $(".model__overlay");
+    var modelDialog = $(".model__dialog");
+    modelOverlay.addClass("model__overlay--visible");
+    modelDialog.addClass("model__dialog--visible");
+  }
+  function closeModel(event) {
+    event.preventDefault();
+    var modelOverlay = $(".model__overlay");
+    var modelDialog = $(".model__dialog");
+    modelOverlay.removeClass("model__overlay--visible");
+    modelDialog.removeClass("model__dialog--visible");
+  }
+
+  $(document).keydown(function (e) {
+    // ESCAPE key pressed
+    if (e.keyCode == 27) {
+      var modalOverlay = $(".model__overlay");
+      var modalDialog = $(".model__dialog");
+      modalOverlay.removeClass("model__overlay--visible");
+      modalDialog.removeClass("model__dialog--visible");
+    }
+  });
 });
