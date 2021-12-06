8a666cd5b2f51a17d3ab0391a345c2eb7d63c20b
zotovalexrew <che123zotov@gmail.com>
2021-12-06 21:35:37 +0300

    create: mask

--- js/main.js
+++ js/main.js
@@ -82,4 +82,9 @@ $(document).ready(function (){
       },
     });
   })
+
+  $(document).ready(function(){
+    $("#phone").mask("+7(000)000-00-00");
+    $("#phone1").mask("+7(000)000-00-00");
+  });
 });
