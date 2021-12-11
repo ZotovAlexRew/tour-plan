8712d14e4373f135e44b484b2f03f8c6f369d62c
zotovalexrew <che123zotov@gmail.com>
2021-12-11 18:52:03 +0300

    fix4

--- js/main.js
+++ js/main.js
@@ -29,7 +29,6 @@ $(document).ready(function (){
 
   var menuButton = document.querySelector(".menu-button");
   menuButton.addEventListener("click", function(){
-    console.log('Клик по кнопке меню');
     document.querySelector(".navbar-button")
     .classList.toggle("navbar-button--visible");
   });
